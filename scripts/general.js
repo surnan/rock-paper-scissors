186336f4292e3add7c45874f3c9ad8741985bc12
surnan <surnan@users.noreply.github.com>
2023-10-05 22:08:00 -0400

    FEATURE: Succesfully console.log winner/loser/tie

--- scripts/general.js
+++ scripts/general.js
@@ -5,11 +5,46 @@
   '.js-scissors-button'
 ].forEach((currentElement) =>
   document.querySelector(currentElement).addEventListener('click', () => {
-    console.log(`hi ${currentElement}`);
+    playGame(currentElement);
   }));
 
+function playGame(playerButton) {
+  const playerAction = playerButton.split("-")[1];
+  const computerAction = getComputerMove(playerAction);
 
-function playGame(temp) {
-  // console.log(temp);
-  console.log('HELLO');
+  let temp = getGameResults(playerAction, computerAction);
+  console.log(`player: ${playerAction} --- action: ${computerAction} -- ${temp}`);
+}
+
+function getGameResults(playerAction, computerAction) {
+  if (playerAction === computerAction) {
+    return 'tie';
+  }
+
+  if (playerAction === 'rock') {
+    if (computerAction === 'paper') {
+      return 'lose';
+    }
+  }
+
+  if (playerAction === 'paper') {
+    if (computerAction === 'scissors') {
+      return 'lose';
+    }
+  }
+
+  if (playerAction === 'scissors') {
+    if (computerAction === 'rock') {
+      return 'lose';
+    }
+  }
+
+  return('win');
+}
+
+function getComputerMove(temp3) {
+  const temp = Math.random();
+  if (temp < 0.33) { return 'rock'; }
+  if (temp > 0.66) { return 'scissors'; }
+  return 'paper'
 }
